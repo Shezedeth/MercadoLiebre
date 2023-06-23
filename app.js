9a25e285093a0c9e6d60985b3c5b3057dc15ff3e
shezedeth@gmail.com <sheze.gomez@gmail.com>
2023-06-23 01:53:19 -0300

    Estructura de un sitio web

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3030;
+
+// CONFIGURACIONES
+
+app.use(express.static("public"));
+
+// RUTAS
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "404.html"))
+);
+
+app.listen(PORT, () =>
+  console.log("Servidor corriendo en http://localhost:" + PORT)
+);
